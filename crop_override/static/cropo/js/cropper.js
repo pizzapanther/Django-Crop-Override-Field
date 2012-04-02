1d2f5117d2e5a3eee151fefccf221e2052e0284a
Paul Bailey <paul.m.bailey@gmail.com>
2012-04-02 16:27:06 -0500

    added preview

--- crop_override/static/cropo/js/cropper.js
+++ crop_override/static/cropo/js/cropper.js
@@ -1,12 +1,26 @@
 var jcrop_api;
-
+var ph = 100;
+var pw = 100;
+  
 function show_cropper (name, orig, aspect) {
   var d = new Date();
   var ts = d.getTime();
   
+  ph = 100;
+  pw = 100;
+
+  if (aspect > 1) {
+    ph = 100 / aspect;
+  }
+  
+  else if (aspect < 1) {
+    pw = 100 * aspect;
+  }
+  
   var img = jQuery('#id_orig_' + orig).val();
-  var html = '<div class="cropper_modal" id="cropper_' + ts + '" style="width: 940px; margin: 0 auto;">\
-<img src="' + img + '" alt="" style="max-width: 940px; max-height: 520px; display: block; margin: 0 auto;"/>\
+  var html = '<div class="cropper_modal" id="cropper_' + ts + '" style="width: 940px;">\
+<div style="width:' + pw + 'px;height:' + ph + 'px;overflow:hidden; margin-top: 25px; float: right;"><img src="' + img + '" class="crop_preview" alt="Preview" style=""></div>\
+<img src="' + img + '" alt="" class="orig" style="max-width: 840px; max-height: 520px; display: block;"/>\
 <br/>\
 <input type="button" name="cancel" value="Cancel" onclick="cancel_cropper()"/> \
 <input type="button" name="done" value="Set Crop" onclick="set_crop(\'' + name + '\')"/>\
@@ -14,7 +28,32 @@ function show_cropper (name, orig, aspect) {
   
   jQuery(html).modal({containerCss: {width: 960, height: 600}});
   
-  jQuery('#cropper_' + ts + ' img').Jcrop({aspectRatio: aspect}, function(){ jcrop_api = this; });
+  jQuery('#cropper_' + ts + ' img.orig').Jcrop(
+    {
+      aspectRatio: aspect,
+      onChange: updatePreview,
+      onSelect: updatePreview,
+    }, function(){ jcrop_api = this; });
+}
+
+function updatePreview (c) {
+  if (parseInt(c.w) > 0) {
+    var rx = pw / c.w;
+    var ry = ph / c.h;
+    
+    var bounds = jcrop_api.getBounds();
+    var boundx = bounds[0];
+    var boundy = bounds[1];
+    
+    console.log(Math.round(rx * c.x));
+    
+    $('.crop_preview').css({
+      width: Math.round(rx * boundx) + 'px',
+      height: Math.round(ry * boundy) + 'px',
+      marginLeft: '-' + Math.round(rx * c.x) + 'px',
+      marginTop: '-' + Math.round(ry * c.y) + 'px'
+    });
+  }
 }
 
 function cancel_cropper () {
